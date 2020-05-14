19d95aa2caff1269c955182dbb57b8fa445e0ecb
David Gao <Davidgao93@gmail.com>
2020-05-14 11:10:04 -0400

    data update

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 del index.bak
 ren index.html index.bak
-ren coronoa-dashboard.html index.html
+ren corona-dashboard.html index.html
 git add --all
 git commit -m "data update"
 git push origin master
